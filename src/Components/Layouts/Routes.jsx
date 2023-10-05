3f6727c66cef39a30d06fd71c74903cc675a4410
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2023-10-04 22:17:15 -0300

    feat(route): add softwares page

--- src/Components/Layouts/Routes.jsx
+++ src/Components/Layouts/Routes.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 //PAGES
 import Home from "../Pages/Home";
 import Membros from "../Pages/Membros";
+import Software from "../Pages/Software";
 
 const RoutesApp = () => {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route exact path="/membros" element={<Membros />} />
+      <Route exact path="/software" element={<Software />} />
     </Routes>
   );
 };
---
+++ src/Components/Pages/Software.jsx
@@ -0,0 +1,71 @@
+import "./Software.css";
+
+import softwareOrcaLogo from "../../assets/imgs/software-orca-logo.png";
+import softwareQuantumEspressoLogo from "../../assets/imgs/software-quantum-espresso-logo.png";
+import logoQuimica from "../../assets/imgs/logoQuimica.jpeg";
+import SoftwareCard from "../Layouts/SoftwareCard";
+
+const Software = () => {
+  return (
+    <main className="home">
+      <div className="bannerHome">
+        <div className="description-banner">
+          <span>Softwares</span>
+          <p>
+            Essa página é dedicada a tutoriais de como utilizar os principais
+            softwares de Química Computacional
+          </p>
+        </div>
+        <div className="banner-img">
+          <img src={logoQuimica} />
+        </div>
+      </div>
+
+      <div className="software-list">
+        {/* Add an article property to SoftwareCard component to render the button */}
+        <SoftwareCard
+          title="Orca"
+          logo={softwareOrcaLogo}
+        >
+          <p>
+            O ORCA é um renomado software de química quântica utilizado para
+            realizar cálculos ab initio avançados em sistemas moleculares e
+            materiais. Desenvolvido por uma equipe de cientistas, o ORCA oferece
+            uma ampla gama de métodos teóricos precisos, permitindo a simulação
+            de propriedades eletrônicas, estruturais e espectroscópicas de
+            moléculas e sólidos. Sua interface amigável e flexibilidade na
+            escolha de métodos e base de dados tornam-no uma ferramenta valiosa
+            para pesquisadores na área da química teórica e computacional. Com
+            uma comunidade ativa e suporte contínuo, o ORCA é amplamente
+            utilizado em estudos acadêmicos e industriais para investigações
+            detalhadas de sistemas químicos complexos.
+          </p>
+        </SoftwareCard>
+
+        <SoftwareCard
+          title="Quantum ESPRESSO"
+          logo={softwareQuantumEspressoLogo}
+          article="ddg.gg"
+        >
+          <p>
+            O Quantum ESPRESSO é um poderoso software de simulação de materiais
+            baseado na teoria do funcional da densidade (DFT) e na teoria do
+            funcional da densidade autoconsistente (DFT-SCF). Projetado para
+            estudar propriedades eletrônicas e estruturais de sólidos e
+            moléculas, o Quantum ESPRESSO oferece uma ampla variedade de métodos
+            numéricos de alta precisão. Sua abordagem de código aberto e
+            modularidade facilita a customização e integração em projetos de
+            pesquisa complexos. Além disso, a plataforma inclui ferramentas para
+            cálculos de dinâmica molecular e espectroscopia, ampliando sua
+            utilidade em diversas áreas da ciência de materiais. Com uma
+            comunidade ativa de usuários e desenvolvedores, o Quantum ESPRESSO é
+            uma ferramenta essencial na modelagem e predição de propriedades de
+            materiais.
+          </p>
+        </SoftwareCard>
+      </div>
+    </main>
+  );
+};
+
+export default Software;
